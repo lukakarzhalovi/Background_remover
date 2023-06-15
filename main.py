c72cc02820ad001b3c0f93b9ffd50dff4ed8e66d
Luka <karzhalovi@gmail.com>
2023-06-15 20:45:31 +0400

    მარტივი როუთინგი

--- main.py
+++ main.py
@@ -4,8 +4,22 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def home():
     return render_template('home.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/sign_in")
+def sign_in():
+    return render_template('sign_in.html')
+
+@app.route("/sign_up")
+def sign_up():
+    return render_template('sign_up.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
