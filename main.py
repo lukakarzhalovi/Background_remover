48d17189cb9a89584a9e641a82f00b6c736b48b0
Luka <karzhalovi@gmail.com>
2023-06-22 20:36:00 +0400

    დავამატე საბაზისო log in და sig up html კოდი და ცვლადებში შევინახე useris შემოყვანილი მონაცემი

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, Request, render_template, redirect ,url_for
+from flask import Flask, request, render_template, redirect ,url_for, session
 from flask_sqlalchemy import SQLAlchemy
 from API import API_remove
 
@@ -14,12 +14,26 @@ def home():
 def about():
     return render_template('about.html')
 
-@app.route("/Log in")
+@app.route("/Log in",  methods = ["POST", "GET" ])
 def sign_in():
+
+    if request.method == "POST":
+        mail = request.form["email"]
+        password = request.form["password"]
+
+        # აქ უნდა შეამოწმო შენს მიერ წაღებული მონაცემი ემთხვევა თუ არა აქ შეყვანილ მონაცემს"
+
     return render_template('Log in.html')
 
 @app.route("/Sign up")
 def sign_up():
+    if request.method == "POST":
+        name = request.form["name"]
+        username = request.form["username"]
+        password = request.form["password"]
+
+        # წიე აქ უნდა დაწერო კონაცემთა ბაზას რო შექმნი მაგის კოდი რო ეს კონაცემები შეინახო ბაზაში
+
     return render_template('Sign up.html')
 
 @app.route("/ImageDetection")
